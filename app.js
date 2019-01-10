8732e191fda9bab908d85c61ab1574dede778ddd
LeoKnox <LeoKnox@users.noreply.github.com>
2019-01-10 15:00:36 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+console.log("Express= ", express);
+var app = express();
+console.log("App = ",app);
+
+app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname + '/views');
+console.log(__dirname);
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('index');
+})
+
+app.get('/cats', function(request, response) {
+    response.render('cats');
+})
+
+app.get('/cars', function(request, response) {
+    response.render('cars');
+})
+
+app.get('/form', function(request, response) {
+    response.render('form');
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
